c3e47b1965b1362dfccd3e764e5d5302133f4e10
idith almog <almog.idith@gmail.com>
2020-04-27 17:38:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+katz_deli=[]
+def line
+  if katz_deli.length=0
+    puts "The line is currently empty."
+  else
+    katz_deli_ordered=[]
+    katz_deli.each_with_index do|name,index| 
+      katz_deli_ordered.push("#{index+1}.#{name}")
+    end
+    puts "The line is currently: #{katz_deli_ordered.join}
+  end
+end
