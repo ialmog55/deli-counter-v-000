251007551e83e138a0736a277970376807f70944
idith almog <almog.idith@gmail.com>
2020-04-27 17:57:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,5 +12,6 @@ def line(katz_deli)
   end
 end
 def take_a_number(katz_deli, name)
-  puts " Welcome #{name} you are number #{katz_deli.length+1} in line"
+  puts "Welcome #{name}. You are number #{katz_deli.length+1} in line."
+  katz_deli.push(name)
 end
